c17019706a54815a4095f118296815fad80439ab
Sergey Dubrovin <sadubrovin1984@gmail.com>
2025-04-13 16:52:53 +0300

    добавление модулей 1,2

---
+++ bornyear.py
@@ -0,0 +1,13 @@
+# (МОДУЛЬ 2) Год рождения А.С.Пушкина
+
+year_of_birth = 1799
+
+while True:
+
+    year = int(input("Введите год рождения Александра Сергеевича Пушкина: "))
+
+    if year == year_of_birth:
+        print("Верно")
+        break
+    else:
+        print("Неверно")
---
+++ variables.py
@@ -0,0 +1,14 @@
+# (МОДУЛЬ 1) В качестве объекта для описания возьмём "овощ"
+
+name = "Тыква" # название овоща
+
+category = 1 # категория качества овоща 1,2,3
+
+weight = 12.2 # масса (вес) овоща
+
+ripe = True # спелый или нет
+
+print(type(name))
+print(type(category))
+print(type(weight))
+print(type(ripe))
