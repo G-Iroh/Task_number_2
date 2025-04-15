0d2f851c76a31853bf04b9a8a9219079f660ee1c
Sergey Dubrovin <sadubrovin1984@gmail.com>
2025-04-15 20:03:59 +0300

    модуль 4 ( ещё вариант)

--- bornyearforewer.py
+++ bornyearforewer.py
@@ -12,22 +12,3 @@ while True:
 
     else:
         print("Неверно")
-
-
-# Вариант с обработкой случая ввода не числового символа
-
-year_of_birth = 1799
-
-while True:
-    try:
-        year = int(input("Введите год рождения Александра Сергеевича Пушкина: "))
-
-        if year == year_of_birth:
-            print("Верно")
-            break
-
-        else:
-            print("Неверно")
-
-    except ValueError:
-        print("Ошибка: Введите число, а не текст!")
